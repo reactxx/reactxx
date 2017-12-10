34e8723acf477af5b30138dbc86380560a779153
Pavel Zika <pzika@langmaster.cz>
2017-12-10 20:40:22 +0100

    v

--- test/index.tsx
+++ test/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom' 
+import ReactDOM from 'react-dom'
 
 import { Play } from 'muix-icons/current/Play'
-import { ruleToClassNames } from 'muix-styles/web/inline-styles' 
+import { ruleToClassNames } from 'muix-styles/web/inline-styles'
 
-const AppComp: React.SFC = props => <h3>{Play} = {MDI.icons.Play} = {ruleToClassNames({})}</h3>
+const AppComp: React.SFC = props => {
+  const classes = ruleToClassNames({ color: 'red' })
+  const classes2 = ruleToClassNames({ color: 'red' })
+  return <h3 className={classes}>{Play} = {MDI.icons.Play} = {classes} = {classes2}</h3>
+}
 
 export const init = () => ReactDOM.render(<AppComp />, document.getElementById('content')) 
