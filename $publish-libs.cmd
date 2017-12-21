750b601363c8fb9239eb4896306a96350f59b8c4
Pavel Zika <pzika@langmaster.cz>
2017-12-21 13:39:33 +0100

    v

--- $publish-libs.cmd
+++ $publish-libs.cmd
@@ -1,9 +1,9 @@
 d:
 cd D:\muix\types-react-native
-call 7z a d:\muix\types-react-native.tar package.json index.d.ts
+call npm pack
 
 cd D:\muix\types-react
-call 7z a d:\muix\types-react.tar package.json index.d.ts
+call npm pack
 
 cd D:\muix\types-expo
-call 7z a d:\muix\types-expo.tar package.json index.d.ts
+call npm pack
