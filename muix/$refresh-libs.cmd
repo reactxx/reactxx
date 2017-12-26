aa3c45bbc56c066c1b7f975888448dcac897c7fd
Pavel Zika <pzika@langmaster.cz>
2017-12-26 10:55:49 +0100

    v

--- muix/$refresh-libs.cmd
+++ muix/$refresh-libs.cmd
@@ -1,4 +1,5 @@
 set root=D:\muix\muix\
+set url=https://github.com/mui-x/muix/blob/master/types/
 
 d:
 
@@ -9,6 +10,7 @@ call jspm install npm:muix-icons
 
 cd %root%
 
+call npm update %url%expo/types-expo-0.1.1.tgz?raw=true %url%react/types-react-0.1.1.tgz?raw=true %url%react-native/types-react-native-0.1.1.tgz?raw=true 
 call npm update muix-icons 
 
 cd %root%node_modules\@types
