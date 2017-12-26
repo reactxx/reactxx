ec7f0daefdb60da8561451950b21e161b28ce680
Pavel Zika <pzika@langmaster.cz>
2017-12-26 11:53:33 +0100

    v

--- muix/$refresh-libs.cmd
+++ muix/$refresh-libs.cmd
@@ -3,17 +3,26 @@ set url=https://github.com/mui-x/muix/blob/master/types/
 
 d:
 
-cd %root%\jspm_packages
-rmdir npm /s /q
-cd %root%
-call jspm install 
-
 cd %root%
 
-call npm update %url%expo/types-expo-0.1.1.tgz?raw=true %url%react/types-react-0.1.1.tgz?raw=true %url%react-native/types-react-native-0.1.1.tgz?raw=true 
-call npm update muix-icons 
+set npm=%root%node_modules\
+rmdir %npm%@types\expo /s /q
+rmdir %npm%@types\react /s /q
+rmdir %npm%@types\react-native /s /q
+rmdir %npm%muix-icons /s /q
+call npm install muix-icons %url%expo/types-expo-0.1.1.tgz?raw=true %url%react/types-react-0.1.1.tgz?raw=true %url%react-native/types-react-native-0.1.1.tgz?raw=true 
 
 cd %root%node_modules\@types
 rmdir node /s /q
+
+
+rem %root%\jspm_packages
+rem rmdir npm /s /q
+rem https://superuser.com/questions/764348/using-wildcards-with-the-rmdir-or-rd-command
+set npm=%root%jspm_packages\npm\
+forfiles /P %npm% /M muix-icons* /C "cmd /c if @isdir==TRUE rmdir /s /q @file"
+
+cd %root%
+call jspm install 
 
 call %root%$compile
