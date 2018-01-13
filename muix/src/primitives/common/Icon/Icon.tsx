a049481e42ba8a52005b021f4e6d6f2f9e52979a
Pavel Zika <pzika@langmaster.cz>
2018-01-13 20:53:46 +0100

    v

--- muix/src/primitives/common/Icon/Icon.tsx
+++ muix/src/primitives/common/Icon/Icon.tsx
@@ -7,17 +7,15 @@ import { IconX } from 'muix-primitives'
 
 import warning from 'warning'
 
-export const iconColor = (color: string) => ({ $native: { color }, $web: { fill: color } } as Muix.TextStyleCommon)
-export const iconSize = (size: number) => ({ $native: { fontSize: size }, $web: { width: size, height: size } } as Muix.TextStyleCommon)
-
 export const sheet = sheetCreator<MuixIcon.Shape>(({ palette }) => ({
-  root: iconSize(24),
-  colorAccent: iconColor(palette.secondary.A200),
-  colorAction: iconColor(palette.action.active),
-  colorContrast: iconColor(palette.getContrastText(palette.primary[500])),
-  colorDisabled: iconColor(palette.action.disabled),
-  colorError: iconColor(palette.error[500]),
-  colorPrimary: iconColor(palette.primary[500]),
+  root: { fontSize: 24 },
+  style: {},
+  colorAccent: { color: palette.secondary.A200 },
+  colorAction: { color: palette.action.active },
+  colorContrast: { color: palette.getContrastText(palette.primary[500]) },
+  colorDisabled: { color: palette.action.disabled },
+  colorError: { color: palette.error[500] },
+  colorPrimary: { color: palette.primary[500] },
   colorInherit: {},
 }))
 
@@ -26,7 +24,7 @@ const icon: Muix.CodeSFC<MuixIcon.Shape> = props => {
   const childs = React.Children.toArray(children)
   warning(childs.length == 1 && typeof childs[0] === 'string', 'single child as string expected')
   const data = childs[0] as MuixIcons
-  const iconClass = getStyleWithSideEffect(classes.root, classes[`color${capitalizeFirstLetter(color)}`])
+  const iconClass = getStyleWithSideEffect(classes.root, classes[`color${capitalizeFirstLetter(color)}`], classes.style)
   return <IconX className={iconClass} style={style} $native={rest as any} $web={rest as any} data={data}/>
 }
 
@@ -34,16 +32,16 @@ const Icon = withStyles<MuixIcon.Shape>(sheet, { name: 'MuiIcon' })(icon)
 
 export default Icon
 
-export const getClasses = <T extends Muix.CSSPropertiesNative | string>({ classes, color = 'inherit', children, theme, innerRef, style, ...rest }: Muix.CodeProps<MuixIcon.Shape>) => {
-  const childs = React.Children.toArray(children)
-  warning(childs.length == 1 && typeof childs[0] === 'string', 'single child as string expected')
-  return {
-    name: childs[0] as MuixIcons,
-    classes: {
-      iconClass: [classes.root, classes[`color${capitalizeFirstLetter(color)}`]] as T[]
-    },
-    rest,
-    style,
-    innerRef
-  }
-}
+//export const getClasses = <T extends Muix.CSSPropertiesNative | string>({ classes, color = 'inherit', children, theme, innerRef, style, ...rest }: Muix.CodeProps<MuixIcon.Shape>) => {
+//  const childs = React.Children.toArray(children)
+//  warning(childs.length == 1 && typeof childs[0] === 'string', 'single child as string expected')
+//  return {
+//    name: childs[0] as MuixIcons,
+//    classes: {
+//      iconClass: [classes.root, classes[`color${capitalizeFirstLetter(color)}`]] as T[]
+//    },
+//    rest,
+//    style,
+//    innerRef
+//  }
+//}
