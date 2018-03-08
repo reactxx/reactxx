12f16ff8c3891377408c10a13c87985b88a0f8e0
Pavel Zika <pzika@langmaster.cz>
2018-03-08 12:08:14 +0100

    v

--- build-icons/$publish.cmd
+++ build-icons/$publish.cmd
@@ -2,12 +2,6 @@ set root=d:\reactxx\build-icons\
 
 d:
 
-copy %root%README.md %root%deploy/README.md
-
 cd %root%deploy
 call npm.cmd version patch
 call npm.cmd publish
-
-rem cd %root%deploy
-rem call yarn link
-
---
+++ build-icons/deploy/README.md
@@ -0,0 +1,2 @@
+# reactxx-mdi
+MDI essentials for both react and react-native (based on https://materialdesignicons.com/)
