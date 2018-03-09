d0387a3229336c3c8bb4157d9dd7ad4e0e1b105d
Pavel Zika <pzika@langmaster.cz>
2018-03-10 00:13:18 +0100

    v

---
+++ reactxx-kitchen-sink/src/typings/theme.d.ts
@@ -0,0 +1,55 @@
+﻿//https://github.com/ReactTraining/react-broadcast/blob/next/modules/createContext.js
+declare namespace ReactXX {
+
+  const enum Consts {
+    //themePropName = 'theme',
+    themeXPropName = 'theme'
+  }
+
+  //type ThemePars = { [name: string]: {} }
+  //type OverridesX = { [name: string]: PartialSheetCreatorX }
+
+  type PartialSheetCreatorX<R extends Shape = Shape> = PartialSheetX<R> | ((themeX: ThemeX, themePar: getThemePar<R>) => PartialSheetX<R>)
+  type SheetCreatorX<R extends Shape = Shape> = SheetX<R> | ((themeX: ThemeX, themePar: getThemePar<R>) => SheetX<R>)
+  type ThemeParCreatorX<R extends Shape = Shape> = getThemePar<R> | ((themeX: ThemeX) => getThemePar<R>)
+  type RulesetCreatorX<R extends Shape = Shape> = RulesetX<getStyle<R>> | ((theme: ThemeX, themePar: getThemePar<R>) => RulesetX<getStyle<R>>)
+
+  interface Theme {
+    type: 'Theme'
+    //direction: Direction
+    //components?: ComponentsTheme
+    //themePars: ThemePars
+    //overridesNew?: FromThemeValueOrCreator<Sheets>
+  }
+  type ThemeX = Partial<Overwrite<Theme, { type: 'ThemeX'}>>
+
+  interface ThemeOptionsX {
+  }
+
+  //type Direction = 'ltr' | 'rtl';
+
+  //type ThemeContextValue = { theme: Theme }
+  //interface ThemeStatesX { theme: Theme; overrides?: OverridesX }
+
+  interface ThemeCompX<R extends Shape = Shape> { override?: PartialSheetX<R>, themePar?: getThemePar<R> }
+  interface ThemeCompSelectedX<R extends Shape = Shape> extends ThemeCompX<R> { theme: ThemeX }
+
+  interface ThemeCompCreatorX<R extends Shape = Shape> { override?: PartialSheetCreatorX<R>, par?: ThemeParCreatorX<R> }
+
+  type ThemeState = { [Consts.themeXPropName]: ThemeX } & {[P in keyof Shapes]?: ThemeCompX<Shapes[P]> }
+  type ThemeModifier = (state: ThemeState) => ThemeState
+
+  //type ThemeStateX<R extends Shape = Shape> = { theme: Theme; override?: PartialSheetX<R> }
+  
+  //interface ThemerProps {
+  //  creator: ThemeCreator
+  //  defaultOptions?: ThemeOptionsX
+  //}
+
+  //type ThemeCreator = (options?: ThemeOptionsX) => Theme
+
+  interface ThemeProviderProps { theme: Theme | ((theme: Theme) => Theme) }
+  //interface AppContainerProps { themerProps: ThemerProps }
+
+
+}
---
+++ reactxx-kitchen-sink/src/typings/typings.d.ts
@@ -0,0 +1,89 @@
+﻿interface Window {
+  isWeb:boolean
+}
+
+declare module 'fela-plugin-rtl' {
+  const def
+  export default def
+}
+
+declare module "fbemitter" {
+  export type EventSubscription = any
+}
+
+declare module 'css-in-js' {
+  export const create
+}
+
+declare module 'jss' {
+  export const create
+  export type GenerateClassName<T> = any
+  export type JSSOptions = any
+  export type StyleSheet<T> = any
+}
+
+declare module 'jss-preset-default' {
+  const def: any
+  export default def
+}
+
+declare module 'invariant' {
+  const invariant: (condition: boolean, msg: string, ...arg: string[]) => void
+  export default invariant
+}
+
+declare module 'hoistNonReactStatics' {
+  export const any
+}
+
+declare module 'url-parse' {
+  interface Query {
+    [index: string]: string;
+  }
+
+  interface QueryParser {
+    (query: string): any;
+  }
+
+  interface ParsedUrl {
+    (url: string, baseURL?: string, parseQuery?: boolean | QueryParser): ParsedUrl;
+    new(url: string, baseURL?: string, parseQuery?: boolean | QueryParser): ParsedUrl;
+
+    protocol: string;   // protocol: Requested protocol without slashes (e.g. http:).
+    username: string;   // username: Username of basic authentication.
+    password: string;   // password: Password of basic authentication.
+    auth: string;       // auth: Authentication information portion (e.g. username:password).
+    host: string;       // host: Host name with port number.
+    hostname: string;   // hostname: Host name without port number.
+    port: string;       // port: Optional port number.
+    pathname: string;   // pathname: URL path.
+    query: Query;       // query: Parsed object containing query string, unless parsing is set to false.
+    hash: string;       // hash: The "fragment" portion of the URL including the pound-sign (#).
+    href: string;       // href: The full URL.
+
+    toString(): string;
+    set(key: string, value: string | Object | number);
+  }
+
+  const URL: ParsedUrl;
+
+  export = URL;
+}
+
+//https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
+type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
+type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]}
+type Overwrite<T, U> = {[P in Diff<keyof T, keyof U>]: T[P]} & U
+type PartialOverwrite<T, U> = {[P in Diff<keyof T, keyof U>]?: T[P]} & Partial<U>
+type TakeFrom<T, K extends keyof T> = {[P in K]: T[P]}
+
+//https://schneidenbach.gitbooks.io/typescript-cookbook/get-the-return-type-of-a-function.html
+//nameOf, return type of function, 
+
+type PartialRecord<K extends string, T> = { [P in K]?: T; };
+
+type OmitFrom<T, K extends string> = {[P in Diff<keyof T, K>]: T[P]}
+
+type NoPartial<T> = {
+  [P in keyof T]: T[P];
+};
