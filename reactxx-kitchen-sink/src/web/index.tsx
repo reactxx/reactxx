b5aa2390196585f88b034e7a3cda36779c19a0ab
Pavel Zika <pzika@langmaster.cz>
2018-03-11 12:19:48 +0100

    v

--- reactxx-kitchen-sink/src/web/index.tsx
+++ reactxx-kitchen-sink/src/web/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 //import App from './root/app';
 //import App from './common/responsible-drawer/responsible-drawer'
-import App from './common/hallo-world';
+import App from '../common/hallo-world';
 // import registerServiceWorker from './root/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
