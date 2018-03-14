9fc6afc2a9e1bf2be5caa196728cf6b9fb36e022
Pavel Zika <pzika@langmaster.cz>
2018-03-14 19:47:40 +0100

    v

--- muix/src/reactxx/native/index.ts
+++ muix/src/reactxx/native/index.ts
@@ -1,11 +1,11 @@
 window.isWeb = false
 
+export * from '../common/index'
+export * from '../common/withStyles'
+export * from '../common/components'
+export * from '../common/theme'
+
 export { Driver as AnimationDriver } from './animation'
 export * from './components'
 export * from './media-q'
 
-export * from '../common/index'
-//export { default as withStyles } from '../common/withStyles'
-export * from '../common/withStyles'
-export * from '../common/components'
-export * from '../common/theme'
