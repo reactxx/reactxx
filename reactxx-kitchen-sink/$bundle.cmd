3496de2a1ce3d64db3746b3a23b1470f27b76db1
Pavel Zika <pzika@langmaster.cz>
2018-03-16 16:34:51 +0100

    v

--- reactxx-kitchen-sink/$bundle.cmd
+++ reactxx-kitchen-sink/$bundle.cmd
@@ -5,3 +5,5 @@ d:
 cd %root%
 
 jspm bundle deploy/lib/web/index.js bundle/index.js --inject --minify --skip-source-maps
+copy D:\reactxx\reactxx-kitchen-sink\jspm.config.js D:\reactxx\docs\jspm.config.js /y
+copy D:\reactxx\reactxx-kitchen-sink\bundle\index.js D:\reactxx\docs\bundle\index.js /y
