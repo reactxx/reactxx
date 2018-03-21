529cb429455548de478c7872d56c4e2183777400
Pavel Zika <pzika@langmaster.cz>
2018-03-21 21:44:47 +0100

    v

---
+++ muix/src/reactxx/typings/index.ts
@@ -0,0 +1,5 @@
+export { TAnimation } from './animation'
+export { TMediaQ } from './media-q'
+export { TTheme } from './theme'
+export { TSheets } from './sheets'
+export * from './typescript-config'
--- muix/src/reactxx/web/index.ts
+++ muix/src/reactxx/web/index.ts
@@ -1,6 +1,8 @@
 export * from '../common/withStyles'
 export * from '../common/theme'
 
+export * from '../typings/index'
+
 export { Driver as AnimationDriver } from './animation'
 export * from './components'
 export * from './media-q'
