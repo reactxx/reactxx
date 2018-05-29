977c6b7f86652111db1f0941fa3f6646081d1b90
Pavel Zika <pzika@langmaster.cz>
2018-05-29 13:37:57 +0200

    v

--- muix/mklink-c.cmd
+++ muix/mklink-c.cmd
@@ -3,7 +3,7 @@ set drive=c:
 del %drive%\reactxx\muix\src\reactxx\typings\types.ts /q
 del %drive%\reactxx\muix\src\mediaq\typings\types.ts  /q
 del %drive%\reactxx\muix\src\animation\typings\types.ts  /q
-rmdir d:\reactxx\muix\src\ks 
+rmdir %drive%\reactxx\muix\src\ks 
 
 mklink %drive%\reactxx\muix\src\reactxx\typings\types.ts %drive%\reactxx\muix\src\basic\typings\types.ts
 mklink %drive%\reactxx\muix\src\mediaq\typings\types.ts %drive%\reactxx\muix\src\basic\typings\types.ts
