5b9213f26f5c613ecfe9dac8012f0df1bb95fc80
Pavel Zika <pzika@langmaster.cz>
2018-07-19 17:05:00 +0200

    all MUI keyframes

--- muix/src/muix/web/styles/withStyles.tsx
+++ muix/src/muix/web/styles/withStyles.tsx
@@ -28,8 +28,9 @@ const withStylesMui = <R extends Types.Shape>
 
 export default withStylesMui
 
-renderer.renderStatic(`
-@-webkit-keyframes mui-ripple-enter {
+// for MUI RIPPLE
+renderer.renderStatic( ['keyframes', '-webkit-keyframes'].map(keyframes => `
+@${keyframes} mui-ripple-enter {
   0% {
     opacity: 0.1;
     transform: scale(0);
@@ -39,7 +40,7 @@ renderer.renderStatic(`
     transform: scale(1);
   }
 }
-@-webkit-keyframes mui-ripple-exit {
+@${keyframes} mui-ripple-exit {
   0% {
     opacity: 1;
   }
@@ -47,7 +48,7 @@ renderer.renderStatic(`
     opacity: 0;
   }
 }
-@-webkit-keyframes mui-ripple-pulsate {
+@${keyframes} mui-ripple-pulsate {
   0% {
     transform: scale(1);
   }
@@ -58,4 +59,66 @@ renderer.renderStatic(`
     transform: scale(1);
   }
 }
-`)
+@${keyframes} mui-progress-circular-rotate: {
+  100%: {
+    transform: rotate(360deg);
+  }
+}
+@${keyframes} mui-progress-circular-dash: {
+  0%: {
+    strokeDasharray: 1px 200px;
+    strokeDashoffset: 0px;
+  }
+  50%: {
+    strokeDasharray: 100px 200px;
+    strokeDashoffset: -15px;
+  }
+  100%: {
+    strokeDasharray: 100px 200px;
+    strokeDashoffset: -120px;
+  }
+
+@${keyframes} mui-indeterminate1: {
+    0%: {
+      left: -35%;
+      right: 100%;
+    }
+    60%: {
+      left: 100%;
+      right: -90%;
+    }
+    100%: {
+      left: 100%;
+      right: -90%;
+    }
+  }
+@${keyframes} mui-indeterminate2: {
+  0%: {
+    left: -200%;
+    right: 100%;
+  }
+  60%: {
+    left: 107%;
+    right: -8%;
+  }
+  100%: {
+    left: 107%;
+    right: -8%;
+  }
+}
+@${keyframes} buffer: {
+  0%: {
+    opacity: 1;
+    backgroundPosition: 0px -23px;
+  }
+  50%: {
+    opacity: 0;
+    backgroundPosition: 0px -23px;
+  }
+  100%: {
+    opacity: 1;
+    backgroundPosition: -200px -23px;
+  }
+}
+
+`).join(''))
