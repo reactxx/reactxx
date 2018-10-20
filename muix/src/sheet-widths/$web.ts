d6f9465017bdc6e95e75cefd0cafa2dbd6707afe
Pavel Zika <pzika@langmaster.cz>
2018-10-20 14:18:32 +0200

    remova maxwidth

--- muix/src/sheet-widths/$web.ts
+++ muix/src/sheet-widths/$web.ts
@@ -1,12 +1,10 @@
-import { maxBreakpoint } from './utils/parser'
 import { onWidthChanged } from './utils/subscribe'
 
 export const actWidth = () => window.innerWidth
 
 export const addBreakpoint = (width: number) => {
-  if (!width || width === maxBreakpoint || widthDir[width]) return
+  if (!width || widthDir[width]) return
   widthDir[width] = true
-  //widths.push(width)
   const mediaQuery = window.matchMedia(`(min-width: ${width}px)`)
   const onChange = (q) => {
     if (!timer) timer = window.setTimeout(() => {
@@ -17,5 +15,4 @@ export const addBreakpoint = (width: number) => {
   mediaQuery.addListener(onChange)
 }
 let timer: number
-//let widths: number[]
 let widthDir: { [width: number]: true }
--- muix/src/sheet-widths/utils/parser.ts
+++ muix/src/sheet-widths/utils/parser.ts
@@ -1,22 +1,20 @@
 import warning = require('warning');
 
-export const maxBreakpoint = 10000000
-
 export const parse = (encoded: string) => {
     warning(regx.test(encoded), `Found '${encoded}, expected e.g '-640' or '1024-' or '640-1024'`)
-    return encoded.split('-').map((i, idx) => i ? parseInt(i) : idx == 0 ? 0 : maxBreakpoint) as [number, number]
+    return encoded.split('-').map(i => i ? parseInt(i) : 0) as [number, number]
 }
 const regx = /^\d+-\d+|-\d+|\d+-$/
 
 export const intervalToSelector = (encoded: string) => {
     const [from, to] = parse(encoded)
-    if (from === 0) return `@media (max-width: ${to - 1}px)`
-    if (to === maxBreakpoint) return `@media (min-width: ${from}px)`
+    if (!from) return `@media (max-width: ${to - 1}px)`
+    if (!to) return `@media (min-width: ${from}px)`
     return `@media (min-width: ${from}px) and (max-width: ${to - 1}px)`
 }
 
 export const test = (interval: [number, number], width) => {
     const [min, max] = interval
-    return min <= width && max < width
+    return min <= width && (!max || max < width)
 }
 
